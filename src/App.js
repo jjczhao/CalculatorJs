8fcd9fcee4e467568ad40ec22d8db625d8b3279f
jjczhao <jasonzhao15@gmail.com>
2019-06-01 00:26:42 -0700

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,111 @@
 import React from 'react';
 import './App.css';
-import Pad from './Pad'
+
 
 
 const numPads = [
-  {padType:'number', symbol: '7'},
-  {padType:'number', symbol: '8'},
-  {padType:'number', symbol: '9'},
-  {padType:'number', symbol: '4'},
-  {padType:'number', symbol: '5'},
-  {padType:'number', symbol: '6'},
-  {padType:'number', symbol: '1'},
-  {padType:'number', symbol: '2'},
-  {padType:'number', symbol: '3'},
+  {symbol: '7', id:"seven"},
+  {symbol: '8', id:"eight"},
+  {symbol: '9', id:"nine"},
+  {symbol: '4', id:"four"},
+  {symbol: '5', id:"five"},
+  {symbol: '6', id:"six"},
+  {symbol: '1', id:"one"},
+  {symbol: '2', id:"two"},
+  {symbol: '3', id:"three"},
+  {symbol: '0', id:"zero"},
+  {symbol:'.', id:"decimal"}
 ];
 
 const operatorPads = [
-  {padType:'operator', symbol: '+'},
-  {padType:'operator', symbol: '-'},
-  {padType:'operator', symbol: '/'},
-  {padType:'operator', symbol: '*'},
-  {padType:'operator', symbol: '='}
+  {symbol: '+',id:"add"},
+  {symbol: '-', id:"subtract"},
+  {symbol: '/', id:"divide"},
+  {symbol: '*', id:"multiply"}
+
 ]
 
+const NUMERIC_FORMAT = /^\d*\.?\d*$/;
 
-function App() {
 
-  const numbers= numPads.map( (x,index) => <Pad key={index} padType={x.padType} symbol={x.symbol}></Pad>)
-  const operators = operatorPads.map( (x,index) => <Pad key={index} padType={x.padType} symbol={x.symbol}></Pad>)
-  
-  return (
-    <div className="App">
-        <div>{numbers}</div>
-        <div>{operators}</div>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      inputs:[],
+      nums:"0",
+      result:"",
+      previousResult:""
+    }
+    this.handleClick = this.handleClick.bind(this);
+    this.handleEvaluation = this.handleEvaluation.bind(this);
+    this.clearInput = this.clearInput.bind(this);
+  }
+
+  handleClick(e){
+    if(e.target.classList[0] === 'number'){
+      let nDigit = e.target.innerHTML.toString();
+      if(this.state.nums.length < 16){
+        if(nDigit !== '.'){
+          if(!this.state.nums.match(/[\+\-\*\/]/)){
+            this.setState({nums: this.state.nums === '0' ? nDigit: this.state.nums + nDigit , result:""});
+          }else{
+            this.setState({inputs:[...this.state.inputs,this.state.nums],nums:nDigit});
+          }
+        }else if(this.state.nums.indexOf('.') < 0){
+          this.setState({nums: this.state.nums + nDigit});
+        }
+      }
+    }else{
+      let nOperator = e.target.innerHTML.toString();
+
+      if(this.state.previousResult !== "" && this.state.inputs.length === 0){
+        this.setState({inputs:[this.state.previousResult],nums:nOperator,result:""});
+      }else if(this.state.nums.match(NUMERIC_FORMAT)){
+
+        this.setState({inputs:[...this.state.inputs,this.state.nums],nums:nOperator});
+      }else{
+
+        this.setState({nums:nOperator});
+      }
+    }
+  }
+
+  clearInput(){
+    this.setState({
+      inputs:[],
+      nums:"0",
+      result:"",
+      previousResult:""
+    });
+  }
+
+  handleEvaluation(){
+    if(this.state.nums.match(NUMERIC_FORMAT)){
+      this.setState({inputs:[...this.state.inputs,this.state.nums]},function(){
+        this.setState({inputs:[],nums:'0',result:eval(this.state.inputs.join(''))}, function(){this.setState({previousResult:this.state.result})});
+      });
+    }
+  }
+
+  render(){
+    const numbers = numPads.map((x,index) => <div id={x.id} className={"number pad item"+index} key={x.symbol} onClick={this.handleClick}>{x.symbol}</div>);
+    const operators = operatorPads.map((x,index) => <div id={x.id} className={"operator pad operator"+index} key={x.symbol} onClick={this.handleClick}>{x.symbol}</div>);
+    return (
+      <div className="App">
+          <div className="pad-container">
+            <div id="display1" className="display">{this.state.inputs}</div>
+            <div id="display" className="display">{this.state.result === "" ? this.state.nums : this.state.result}</div>
+              <div id="clear" className="clear pad" onClick={this.clearInput}>AC</div>
+              {numbers}
+              {operators}
+              <div id="equals" className="equal pad" onClick={this.handleEvaluation}>=</div>
+          </div>
+          
+      </div>
+    );  
+  }
 }
 
 export default App;
